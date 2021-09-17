1d8dbb0d8e61ab56fcb856836bd03a64eb4d90eb
Andreas Manousakis <and.manousakis@gmail.com>
2021-09-17 03:38:49 +0300

    Updating core.py, environment.yml

--- core.py
+++ core.py
@@ -1,10 +1,10 @@
 # Dependencies.
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
+#from sklearn.model_selection import train_test_split
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.metrics import accuracy_score
+#from sklearn.preprocessing import StandardScaler
 import streamlit as st
 
 # Importing dataset.
